53e4fa78f760741d051f71529c46ceccd244275b
Santosh Kumar Subedi <subedisantos9@gmail.com>
2018-04-06 23:59:26 +0545

    list is fun april 6

---
+++ list.py
@@ -0,0 +1,18 @@
+# # is used in python to write comment
+# declaring variable in python is same as java script
+# datatype is determine by formate of data
+
+
+beverage=["coffee","ted"]
+#printing whole list at once
+print(beverage)
+#printing item from list using index
+print(beverage[1])
+#adding a new item
+beverage.append("hot-lemone")
+        #printing beverage after adding hot-lemone
+print(beverage)
+# poping a value and assigning it to new variable
+first=beverage.pop()
+#pop take last element from the list
+print(first)
